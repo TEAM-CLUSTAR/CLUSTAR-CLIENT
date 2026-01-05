86ead03531928a882c11f15b61e362520ba620a2
jyeon03 <bjlyeon2003@naver.com>
2026-01-05 18:00:50 +0900

    init: RouterProvider 적용

--- apps/client/src/apps/App.tsx
+++ apps/client/src/apps/App.tsx
@@ -1,11 +1,8 @@
-import { Button } from '@cds/ui';
+import { RouterProvider } from 'react-router';
+import { router } from '@shared/router';
 
 function App() {
-  return (
-    <>
-      <Button />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
