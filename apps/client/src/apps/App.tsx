3fc514cdaaeccc0d671649b2ad5386344f246c1f
twossu <nadao202@naver.com>
2026-01-06 00:11:49 +0900

    init: 에러 바운더리 및 suspense 설정

--- apps/client/src/apps/App.tsx
+++ apps/client/src/apps/App.tsx
@@ -1,16 +1,21 @@
 import { Button } from '@cds/ui';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
+import { useState, Suspense } from 'react';
 import { createQueryClient } from '../shared/api/query-client'; //@TODO 절대경로 바꾸기
+import { ErrorBoundary } from 'react-error-boundary';
 
 function App() {
   const [client] = useState(() => createQueryClient());
 
   return (
     <QueryClientProvider client={client}>
-      <Button />
       <ReactQueryDevtools initialIsOpen={false} />
+      <ErrorBoundary fallback={<h1>에러 발생</h1>}>
+        <Suspense fallback={<h1>로딩 중..</h1>}>
+          <Button />
+        </Suspense>
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 }
