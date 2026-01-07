2712ede0e4da42cfb2371b1376913dc726260956
jm8468 <jmtoyplay1113@gmail.com>
2026-01-07 21:54:53 +0900

    docs: packages/api 폴더 및 하위 파일 삭제

--- packages/api/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { config } from '@clustar/eslint-config/react-internal';
-
-/** @type {import("eslint").FlatConfig[]} */
-export default config;
--- packages/api/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { createClient } from './client.js';
-export type { BaseResponse } from './types.js';
-export * from 'axios';
