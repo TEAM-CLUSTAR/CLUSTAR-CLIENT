37fbb55584bfbe917c7bb90a60b202d140fd1cb7
jm8468 <jmtoyplay1113@gmail.com>
2026-01-07 21:56:12 +0900

    init: apps/client 내에서 axios 설치 및 세팅

--- apps/client/src/shared/api/instance.ts
+++ apps/client/src/shared/api/instance.ts
@@ -1,5 +1,5 @@
-import { createClient } from '@cds/api';
+import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-export const api = createClient(BASE_URL);
+export const instance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+});
