a367e03cdaba0fd9104b27f1569102e6711e46d8
jm8468 <jmtoyplay1113@gmail.com>
2026-01-09 07:33:08 +0900

    init: type safe를 위한 기능 추가

---
+++ packages/cds-ui/src/icons/icon-list.ts
@@ -0,0 +1,23 @@
+// 이 파일은 자동 생성 파일입니다. 직접 수정하지 마세요!
+export const iconNames = [
+  'ic_ai',
+  'ic_allmemo',
+  'ic_arrow-left',
+  'ic_breif',
+  'ic_cardview',
+  'ic_close',
+  'ic_draft',
+  'ic_file',
+  'ic_folding',
+  'ic_img',
+  'ic_newmemo',
+  'ic_organize',
+  'ic_output',
+  'ic_pdf',
+  'ic_profile',
+  'ic_search',
+  'ic_tag',
+  'ic_trash',
+  'ic_treeview',
+] as const;
+export type IconName = (typeof iconNames)[number];
