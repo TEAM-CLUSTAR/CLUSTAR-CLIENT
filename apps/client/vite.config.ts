af7a90bff7b48d5158c8831e76941519fb3f8ced
jyeon03 <bjlyeon2003@naver.com>
2026-01-08 17:18:58 +0900

    init: Vitest 설정

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+  },
 });
