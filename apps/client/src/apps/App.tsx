57d80839824f914b28e685e20afab2462c594571
최윤하 <nadao202@naver.com>
2026-01-08 18:11:28 +0900

    Init(client): tanstack query 초기 세팅 (#27)
    * init: TanStack Query 설치
    
    * init: 리액트 쿼리 초기세팅
    
    * fix: utils 폴더에서 api로 이동
    
    * chore: 에러바운더리 설치
    
    * init: 에러 바운더리 및 suspense 설정
    
    * init: 에러바운더리 설치
    
    * chore: client/package 의존성 수정
    
    * fix: refetchOnWindowFocus 옵션 true로 변경
    
    * chore: 에러바운더리 제거
    
    * fix: retry 옵션 동적 설정
    
    * chore: pnpm-lock.yaml 업데이트
    
    * fix: createQueryClient 함수 내 환경 변수 선언 위치 수정
    
    * fix: package.json에서 @tanstack/react-query 항목 뒤에 쉼표 추가
    
    * fix: createQueryClient 함수에서 환경 변수 처리 방식 수정

--- apps/client/src/apps/App.tsx
+++ apps/client/src/apps/App.tsx
@@ -1,8 +1,19 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
+import { createQueryClient } from '@shared/api/query-client';
 import { router } from '@shared/router/router';
 import { RouterProvider } from 'react-router';
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [client] = useState(() => createQueryClient());
+
+  return (
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
