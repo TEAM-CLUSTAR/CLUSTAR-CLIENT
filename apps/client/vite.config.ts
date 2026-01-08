65237176688b45e56b9af734ed80521d78bd7f73
jogpfls <joerin4177@gmail.com>
2026-01-09 03:07:38 +0900

    chore: @sentry/vite-plugin 추가

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react-swc';
 
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import { sentryVitePlugin } from '@sentry/vite-plugin';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      org: 'your-org',
+      project: 'your-project',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
 });
