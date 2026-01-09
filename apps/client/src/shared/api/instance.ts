5a0677e6ae6b14674eb5ba1dc3e910f768b5098e
jogpfls <joerin4177@gmail.com>
2026-01-09 18:03:49 +0900

    chore: instance 변수를 export로 변경하여 모듈화 개선

--- apps/client/src/shared/api/instance.ts
+++ apps/client/src/shared/api/instance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { SentryInterceptor } from './interceptors/sentry.interceptor';
 
-export const instance = axios.create({
+const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
