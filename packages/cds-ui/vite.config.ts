bfa104e9f12b337c2600354b4f4b6a3246ec495c
백지연 <147830921+jyeon03@users.noreply.github.com>
2026-01-09 17:43:43 +0900

    Init(project): 테스트코드 세팅 (#35)
    * init: 테스트코드관련 패키지 설치
    
    * init: Vitest 설정
    
    * init: TypeScript 설정
    
    * init: 테스트 스크립트 추가
    
    * init: 테스트 전역 설정
    
    * feat: button 테스트코드 예시
    
    * init: @vitest/ui 패키지 설치
    
    * fix: lint 오류 해결
    
    * feat: 테스트코드 카탈로그 적용
    
    * fix: lefthook 수정
    
    * feat: 루트 package.json에 test 스크립트 추가
    
    * feat: test와 test:run 태스크 추가
    
    * feat: 테스트 실행 단계 추가
    
    * fix: 테스트코드 타입 체크 중복 코드 제거
    
    * fix: --passWithNoTests 플래그 추가

--- packages/cds-ui/vite.config.ts
+++ packages/cds-ui/vite.config.ts
@@ -1,8 +1,14 @@
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react({})],
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
+  },
   build: {
     lib: {
       entry: 'src/components/index.ts',
