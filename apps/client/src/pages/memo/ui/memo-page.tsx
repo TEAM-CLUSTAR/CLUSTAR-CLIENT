f688e266c25f6942475e5d4a0565d4a933b354c5
jm8468 <jmtoyplay1113@gmail.com>
2026-01-10 00:11:08 +0900

    fix: 예제 컴포넌트 import 삭제

--- apps/client/src/pages/memo/ui/memo-page.tsx
+++ apps/client/src/pages/memo/ui/memo-page.tsx
@@ -1,11 +1,9 @@
 import { Icon } from '@cds/icon';
-import { Button } from '@cds/ui';
 
 const MemoPage = () => {
   return (
     <div>
       <Icon name="ic_ai_gra" />
-      <Button />
     </div>
   );
 };
