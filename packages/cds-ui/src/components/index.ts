cc81ca2d690f7316dcebf103cb47b97236b58224
twossu <nadao202@naver.com>
2026-01-10 02:35:50 +0900

    feat: 페이지 타이틀 ui 구현

--- packages/cds-ui/src/components/index.ts
+++ packages/cds-ui/src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Button } from './button/button';
+export { default as PageTitle } from './page-title/page-title';
---
+++ packages/cds-ui/src/components/page-title/page-title.css.ts
@@ -0,0 +1,26 @@
+import { style } from '@vanilla-extract/css';
+
+import { themeVars } from '../../styles';
+
+export const container = style({
+  display: 'flex',
+  gap: '1.1rem',
+  alignItems: 'center',
+});
+
+export const title = style({
+  ...themeVars.fontStyles.title_sb_24,
+  color: themeVars.color.grey900,
+});
+
+export const count = style({
+  display: 'flex',
+  height: '2.5rem',
+  padding: '0 0.8rem',
+  backgroundColor: themeVars.color.blue100,
+  border: `1px solid ${themeVars.color.blue300}`,
+  borderRadius: '10rem',
+  ...themeVars.fontStyles.body_m_14,
+  color: themeVars.color.blue500,
+  alignItems: 'center',
+});
