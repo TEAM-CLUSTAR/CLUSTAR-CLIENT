e1891a00ecfd4bdcffaab4c6bf54288971d86c14
jyeon03 <bjlyeon2003@naver.com>
2026-01-11 03:34:00 +0900

    fix: 버튼 쉐도우 추가

--- packages/cds-ui/src/components/fab/fab.css.ts
+++ packages/cds-ui/src/components/fab/fab.css.ts
@@ -24,6 +24,8 @@ export const button = recipe({
     mode: {
       default: {
         backgroundColor: themeVars.color.blue500,
+        boxShadow:
+          '0 1rem 1.5rem -0.3rem rgba(0, 0, 0, 0.10), 0 0.4rem 0.6rem -0.4rem rgba(0, 0, 0, 0.10)',
         selectors: {
           '&:hover': {
             backgroundColor: themeVars.color.blue700,
@@ -32,6 +34,7 @@ export const button = recipe({
       },
       active: {
         backgroundColor: 'transparent',
+        boxShadow: '0 0 1rem 0 rgba(34, 15, 93, 0.40)',
         selectors: {
           '&::before': {
             position: 'absolute',
