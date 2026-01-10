3e082aced7551ed6b6a69f8afde0e01014d2d4ef
jyeon03 <bjlyeon2003@naver.com>
2026-01-10 20:46:50 +0900

    feat: fab 컴포넌트 storybook 작성

---
+++ packages/cds-ui/src/components/fab/fab.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Fab from './fab';
+
+const meta: Meta<typeof Fab> = {
+  title: 'Components/Fab',
+  component: Fab,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Active: Story = {
+  render: () => <Fab isActive={true} onClick={() => {}} />,
+};
