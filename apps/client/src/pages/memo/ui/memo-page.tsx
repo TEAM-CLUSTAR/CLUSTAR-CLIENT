2b892227cf3018d83434e7a634e5b875a2acbdf6
twossu <nadao202@naver.com>
2026-01-10 21:57:27 +0900

    feat: 스토리북 코드 작성

--- apps/client/src/pages/memo/ui/memo-page.tsx
+++ apps/client/src/pages/memo/ui/memo-page.tsx
@@ -1,14 +1,9 @@
 import { Icon } from '@cds/icon';
-import { ImgContainer } from '@cds/ui';
 
 const MemoPage = () => {
   return (
     <div>
       <Icon name="ic_ai_gra" />
-      <ImgContainer
-        src="https://image.cine21.com/resize/cine21/article/2005/1125/K0000030_main[H800-].jpg"
-        alt="이미지"
-      />
     </div>
   );
 };
