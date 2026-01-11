1e8333f2cd9f3607d80c2d2259e89a789da70eb9
jogpfls <joerin4177@gmail.com>
2026-01-12 05:30:33 +0900

    feat: 사용자 정보 텍스트 스타일 개선

--- packages/cds-ui/src/components/sidebar-profile/sidebar-profile.css.ts
+++ packages/cds-ui/src/components/sidebar-profile/sidebar-profile.css.ts
@@ -38,14 +38,21 @@ export const sidebarProfileContainer = style({
 export const userInfoTextContainer = style({
   display: 'flex',
   flexDirection: 'column',
+  minWidth: 0,
 });
 
 export const userId = style({
   ...themeVars.fontStyles.body_sb_16,
   color: themeVars.color.grey700,
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
 });
 
 export const userEmail = style({
   ...themeVars.fontStyles.label_m_12,
   color: themeVars.color.grey500,
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
 });
