c24957ddd35e381f16224f7001b06722e158ac05
twossu <nadao202@naver.com>
2026-01-12 04:27:12 +0900

    fix: memopage 내용 초기화

--- apps/client/src/pages/memo/ui/memo-page.tsx
+++ apps/client/src/pages/memo/ui/memo-page.tsx
@@ -1,11 +1,9 @@
 import { Icon } from '@cds/icon';
-import { ImgContainer } from '@cds/ui';
 
 const MemoPage = () => {
   return (
     <div>
       <Icon name="ic_ai_gra" />
-      <ImgContainer imageUrl="image.png" imageAlt="설명" />
     </div>
   );
 };
