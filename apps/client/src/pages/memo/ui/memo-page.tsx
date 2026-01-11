f8ff8f999e9d880fdec3c4955bf45556666d6a02
twossu <nadao202@naver.com>
2026-01-12 04:24:12 +0900

    chore: props 명 변경

--- apps/client/src/pages/memo/ui/memo-page.tsx
+++ apps/client/src/pages/memo/ui/memo-page.tsx
@@ -1,9 +1,11 @@
 import { Icon } from '@cds/icon';
+import { ImgContainer } from '@cds/ui';
 
 const MemoPage = () => {
   return (
     <div>
       <Icon name="ic_ai_gra" />
+      <ImgContainer imageUrl="image.png" />
     </div>
   );
 };
