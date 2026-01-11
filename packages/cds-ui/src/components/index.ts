cbfc4c69b92445312c6ee700c4971a28991f785a
조혜린 <164613169+jogpfls@users.noreply.github.com>
2026-01-12 05:43:33 +0900

    Feat(cds): 사이드바 프로필 컴포넌트 구현 (#64)
    * feat: 사이드바 프로필 컴포넌트 구현
    
    * feat: 스토리북 작성
    
    * feat: 사용자 정보 텍스트 스타일 개선
    
    * fix: 아이콘 컴포넌트의 속성 수정
    
    * fix: 사이드바 프로필 zIndex 값 수정
    
    * fix: userInfoTextContainer의 flex 속성 추가
    
    * fix: userId 및 userEmail 스타일에 ellipsisStyle 적용

--- packages/cds-ui/src/components/index.ts
+++ packages/cds-ui/src/components/index.ts
@@ -6,3 +6,4 @@ export { default as Label } from './label/label';
 export { default as PageTitle } from './page-title/page-title';
 export { default as Search } from './search/search';
 export { default as SelectedMemo } from './selected-memo/selected-memo';
+export { default as SideBarProfile } from './sidebar-profile/sidebar-profile';
---
+++ packages/cds-ui/src/components/sidebar-profile/sidebar-profile.css.ts
@@ -0,0 +1,61 @@
+import { style } from '@vanilla-extract/css';
+
+import { themeVars } from '../../styles';
+
+export const sidebarProfileContainer = style({
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.5rem',
+  width: '22rem',
+  padding: '1rem 0.4rem',
+  borderRadius: '8px',
+  backgroundColor: 'transparent',
+  selectors: {
+    '&::before': {
+      position: 'absolute',
+      inset: '-2px',
+      zIndex: themeVars.zIndex.deep,
+      background: themeVars.color.gradient02,
+      borderRadius: '8px',
+      pointerEvents: 'none',
+      content: '',
+    },
+    '&::after': {
+      position: 'absolute',
+      inset: '-2px',
+      zIndex: themeVars.zIndex.back,
+      border: '1px solid transparent',
+      background: themeVars.color.gradient03,
+      borderRadius: '8px',
+      backgroundClip: 'content-box',
+      pointerEvents: 'none',
+      content: '',
+    },
+  },
+});
+
+export const userInfoTextContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  minWidth: 0,
+  flex: 1,
+});
+
+const ellipsisStyle = {
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+};
+
+export const userId = style({
+  ...themeVars.fontStyles.body_sb_16,
+  color: themeVars.color.grey700,
+  ...ellipsisStyle,
+});
+
+export const userEmail = style({
+  ...themeVars.fontStyles.label_m_12,
+  color: themeVars.color.grey500,
+  ...ellipsisStyle,
+});
