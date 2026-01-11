8efeb8d7e979633e43200ebdf1233d106f03aaef
jm8468 <jmtoyplay1113@gmail.com>
2026-01-12 05:29:50 +0900

    fix: 코드리뷰 반영
    padding의 px 를 rem으로 변경
    height 삭제

--- packages/cds-ui/src/components/hover-label-token/hover-label-token.css.ts
+++ packages/cds-ui/src/components/hover-label-token/hover-label-token.css.ts
@@ -3,10 +3,9 @@ import { style } from '@vanilla-extract/css';
 import { themeVars } from '../../styles';
 
 export const container = style({
-  padding: '0px 6px',
+  padding: '0 0.6rem',
   ...themeVars.fontStyles.body_sb_14,
   color: themeVars.color.grey700,
-  height: '2.1rem',
   borderRadius: '4px',
 
   ':hover': {
