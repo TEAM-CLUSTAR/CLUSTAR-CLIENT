e63543dd4156bd7551d42a834e706a70a541b371
조혜린 <164613169+jogpfls@users.noreply.github.com>
2026-01-12 05:22:46 +0900

    Feat(cds): 라벨 컴포넌트 구현 (#61)
    * fix: label에 해당하는 색상 변수명 변경
    
    * feat: 라벨 컴포넌트 구현
    
    * feat: 라벨 컴포넌트 스토리 추가
    
    * fix: TypeScript 오류 주석 제거 및 메타 타입 명시
    
    * fix: labelIndicator의 borderRadius 속성을 variants로 분기
    
    * fix: Label 컴포넌트의 루트 요소를 label에서 div로 변경

--- packages/cds-ui/src/components/index.ts
+++ packages/cds-ui/src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as Button } from './button/button';
 export { default as FloatingButton } from './floating-button/floating-button';
 export { default as ImgContainer } from './image-container/image-container';
+export { default as Label } from './label/label';
 export { default as PageTitle } from './page-title/page-title';
 export { default as Search } from './search/search';
 export { default as SelectedMemo } from './selected-memo/selected-memo';
