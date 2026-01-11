223f823cbc21ffc614210afa857b059fa41625ad
백지연 <147830921+jyeon03@users.noreply.github.com>
2026-01-12 05:02:07 +0900

    Feat(cds): FAB 컴포넌트 구현 (#57)
    * feat: fab 컴포넌트 구현
    
    * feat: fab 컴포넌트 storybook 작성
    
    * fix: FabProps 수정
    
    * fix: 버튼 쉐도우 추가
    
    * fix: fab 파일명 변경
    
    * fix: isActive boolean variant로 변경
    
    * fix: children props 추가
    
    * fix: borderRadius 단위 변경
    
    * fix: 스타일 코드 수정
    
    * feat: z-index 토큰화
    
    * fix: transition 추가
    
    * fix: z-index 상수명 변경
    
    * fix: floating-button 폴더명 변경

--- packages/cds-ui/src/components/index.ts
+++ packages/cds-ui/src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Button } from './button/button';
+export { default as FloatingButton } from './floating-button/floating-button';
 export { default as ImgContainer } from './image-container/image-container';
-export { default as SelectedMemo } from './selected-memo/selected-memo';
 export { default as PageTitle } from './page-title/page-title';
 export { default as Search } from './search/search';
-
+export { default as SelectedMemo } from './selected-memo/selected-memo';
