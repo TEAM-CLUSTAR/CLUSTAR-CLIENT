372f72863397cbd6bb1f7a4200d075f11c5afa28
jyeon03 <bjlyeon2003@naver.com>
2026-01-12 04:39:25 +0900

    fix: borderRadius 단위 변경

--- packages/cds-ui/src/components/button/button.css.ts
+++ packages/cds-ui/src/components/button/button.css.ts
@@ -5,7 +5,7 @@ import { themeVars } from '../../styles';
 export const button = style({
   width: '100%',
   height: '5.2rem',
-  borderRadius: '1.2rem',
+  borderRadius: '12px',
   backgroundColor: themeVars.color.blue500,
   color: themeVars.color.white,
   ...themeVars.fontStyles.title_sb_18,
