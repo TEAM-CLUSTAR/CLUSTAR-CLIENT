6b9ace445355f4b090263d887c8b92dfb2345910
jyeon03 <bjlyeon2003@naver.com>
2026-01-13 05:38:41 +0900

    fix: 버튼 구조 수정

--- packages/cds-ui/src/components/button/button.tsx
+++ packages/cds-ui/src/components/button/button.tsx
@@ -1,7 +1,5 @@
 import type { ReactNode } from 'react';
 
-import { Icon } from '@cds/icon';
-
 import * as styles from './button.css';
 
 export type ButtonSizeType = 'sm' | 'md' | 'lg';
@@ -22,22 +20,21 @@ const Button = ({
   variant = 'solid',
   disabled = false,
 }: ButtonProps) => {
-  const renderContent = () => {
-    if (size === 'sm') return <Icon name="ic_send" />;
-    if (size === 'md' && variant === 'outlined') {
-      return <span className={styles.outlinedText}>{children}</span>;
-    }
-    return children;
-  };
+  const applyOutlinedText =
+    size === 'md' && variant === 'outlined' && typeof children === 'string';
 
   return (
     <button
       type="button"
       onClick={onClick}
-      disabled={size === 'sm' && disabled}
+      disabled={disabled}
       className={styles.button({ size, variant, disabled })}
     >
-      {renderContent()}
+      {applyOutlinedText ? (
+        <span className={styles.outlinedText}>{children}</span>
+      ) : (
+        children
+      )}
     </button>
   );
 };
