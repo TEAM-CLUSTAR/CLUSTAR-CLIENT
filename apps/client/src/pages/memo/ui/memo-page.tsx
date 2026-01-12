dcef91420c318a8132cf0234c8c2ba0cd10591f6
twossu <nadao202@naver.com>
2026-01-12 23:21:13 +0900

    fix: 메모 페이지 테스트한 코드 삭제

--- apps/client/src/pages/memo/ui/memo-page.tsx
+++ apps/client/src/pages/memo/ui/memo-page.tsx
@@ -1,29 +1,9 @@
-import { useState } from 'react';
-
 import { Icon } from '@cds/icon';
-import { SidebarPannel } from '@cds/ui';
 
 const MemoPage = () => {
-  const [isSelected, setIsSelected] = useState(false);
-  const handleClick = () => {
-    setIsSelected((prev) => !prev);
-  };
   return (
     <div>
       <Icon name="ic_ai_gra" />
-      <SidebarPannel
-        isSelected={isSelected}
-        onClick={handleClick}
-        icon={
-          isSelected ? (
-            <Icon name="ic_newmemo_blue" width={36} height={36} />
-          ) : (
-            <Icon name="ic_newmemo" width={36} height={36} />
-          )
-        }
-      >
-        새 메모
-      </SidebarPannel>
     </div>
   );
 };
