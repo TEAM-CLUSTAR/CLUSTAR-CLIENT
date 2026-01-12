0fdabb47ccf4f9403111f5e9ac119e429f9cc87b
twossu <nadao202@naver.com>
2026-01-12 22:23:51 +0900

    fix: 사이드패널에서 아이콘 받아오는 방식 변경

--- apps/client/src/pages/memo/ui/memo-page.tsx
+++ apps/client/src/pages/memo/ui/memo-page.tsx
@@ -1,19 +1,27 @@
+import { useState } from 'react';
+
 import { Icon } from '@cds/icon';
 import { SidebarPannel } from '@cds/ui';
 
 const MemoPage = () => {
+  const [isSelected, setIsSelected] = useState(false);
+  const handleClick = () => {
+    setIsSelected((prev) => !prev);
+  };
   return (
     <div>
       <Icon name="ic_ai_gra" />
       <SidebarPannel
-        iconName="ic_newmemo"
-        isSelected={false}
-        onClick={() => {}}
+        isSelected={isSelected}
+        onClick={handleClick}
+        icon={
+          isSelected ? (
+            <Icon name="ic_newmemo_blue" width={36} height={36} />
+          ) : (
+            <Icon name="ic_newmemo" width={36} height={36} />
+          )
+        }
       >
-        새 메모
-      </SidebarPannel>
-
-      <SidebarPannel iconName="ic_newmemo" isSelected={true} onClick={() => {}}>
         새 메모
       </SidebarPannel>
     </div>
--- packages/cds-ui/src/components/sidebar-pannel/sidebar-pannel.tsx
+++ packages/cds-ui/src/components/sidebar-pannel/sidebar-pannel.tsx
@@ -1,25 +1,20 @@
 import type { ReactNode } from 'react';
 
-import { Icon } from '@cds/icon';
-import { IconName } from '@cds/icon';
-
 import * as styles from './sidebar-pannel.css';
 
 interface SideBarPannelProps {
   children: ReactNode;
   isSelected: boolean;
-  iconName: IconName;
   onClick: () => void;
+  icon: ReactNode;
 }
 
 const SidebarPannel = ({
   children,
   isSelected,
-  iconName,
   onClick,
+  icon,
 }: SideBarPannelProps) => {
-  const currentState = (isSelected ? `${iconName}_blue` : iconName) as IconName;
-
   return (
     <button
       type="button"
@@ -27,7 +22,7 @@ const SidebarPannel = ({
       aria-selected={isSelected}
       onClick={onClick}
     >
-      <Icon name={currentState} width={36} height={36} />
+      {icon}
       {children}
     </button>
   );
