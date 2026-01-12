3e1a1d4c1e2069edc31cf4acd9b538081af35456
twossu <nadao202@naver.com>
2026-01-13 05:26:11 +0900

    chore: 메모 페이지 원래 상태로 복원

--- apps/client/src/pages/memo/ui/memo-page.tsx
+++ apps/client/src/pages/memo/ui/memo-page.tsx
@@ -1,27 +1,9 @@
-import { useState } from 'react';
-
 import { Icon } from '@cds/icon';
-import { SidebarIcon } from '@cds/ui';
 
 const MemoPage = () => {
-  const [isSelected, setIsSelected] = useState(false);
-  const handleClick = () => {
-    setIsSelected((prev) => !prev);
-  };
   return (
     <div>
       <Icon name="ic_ai_gra" />
-      <SidebarIcon
-        isSelected={isSelected}
-        onClick={handleClick}
-        icon={
-          isSelected ? (
-            <Icon name="ic_newmemo_blue" width={36} height={36} />
-          ) : (
-            <Icon name="ic_newmemo" width={36} height={36} />
-          )
-        }
-      />
     </div>
   );
 };
