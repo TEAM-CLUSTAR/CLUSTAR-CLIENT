9058875c8cf5aa85061d67b4344fa2ff5d131d5b
jyeon03 <bjlyeon2003@naver.com>
2026-01-14 02:11:21 +0900

    feat: header 스토리북 수정

--- packages/cds-ui/src/components/header/header.stories.tsx
+++ packages/cds-ui/src/components/header/header.stories.tsx
@@ -36,13 +36,15 @@ const DefaultHeader = () => {
       handleChangeInput={handleChangeInput}
       viewMode={viewMode}
       handleValueChange={handleValueChange}
+      isTitleVisible={true}
+      isToggleVisible={true}
     />
   );
 };
 
 /**
  * 기본 Header 컴포넌트입니다.
- * PageTitle, Search, Toggle을 포함한 전체 헤더 레이아웃입니다.
+ * PageTitle, Search, Toggle을 모두 포함한 전체 헤더 레이아웃입니다.
  */
 export const Default: Story = {
   render: () => <DefaultHeader />,
@@ -50,7 +52,7 @@ export const Default: Story = {
 
 const MemoListHeader = () => {
   const [inputValue, setInputValue] = useState('');
-  const [viewMode, setViewMode] = useState<ViewType>('tree');
+  const [viewMode, setViewMode] = useState<ViewType>('card');
 
   const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -68,6 +70,8 @@ const MemoListHeader = () => {
       handleChangeInput={handleChangeInput}
       viewMode={viewMode}
       handleValueChange={handleValueChange}
+      isTitleVisible={true}
+      isToggleVisible={true}
     />
   );
 };
@@ -78,6 +82,92 @@ const MemoListHeader = () => {
  */
 export const MemoList: Story = {
   render: () => <MemoListHeader />,
+};
+
+const TitleOnlyHeader = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <Header
+      title="전체 메모"
+      count={10}
+      inputValue={inputValue}
+      handleChangeInput={handleChangeInput}
+      isTitleVisible={true}
+      isToggleVisible={false}
+    />
+  );
+};
+
+/**
+ * PageTitle만 표시하는 Header 예시입니다.
+ * Toggle은 숨겨지고 Search와 PageTitle만 보입니다.
+ */
+export const TitleOnly: Story = {
+  render: () => <TitleOnlyHeader />,
+};
+
+const ToggleOnlyHeader = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [viewMode, setViewMode] = useState<ViewType>('card');
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleValueChange = (value: string) => {
+    setViewMode(value as ViewType);
+  };
+
+  return (
+    <Header
+      title="전체 메모"
+      count={10}
+      inputValue={inputValue}
+      handleChangeInput={handleChangeInput}
+      viewMode={viewMode}
+      handleValueChange={handleValueChange}
+      isTitleVisible={false}
+      isToggleVisible={true}
+    />
+  );
+};
+
+/**
+ * Toggle만 표시하는 Header 예시입니다.
+ * PageTitle은 숨겨지고 Search와 Toggle만 보입니다.
+ */
+export const ToggleOnly: Story = {
+  render: () => <ToggleOnlyHeader />,
+};
+
+const SearchOnlyHeader = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <Header
+      inputValue={inputValue}
+      handleChangeInput={handleChangeInput}
+      isTitleVisible={false}
+      isToggleVisible={false}
+    />
+  );
+};
+
+/**
+ * Search만 표시하는 Header 예시입니다.
+ * PageTitle과 Toggle이 모두 숨겨진 최소한의 헤더입니다.
+ */
+export const SearchOnly: Story = {
+  render: () => <SearchOnlyHeader />,
 };
 
 const EmptySearchHeader = () => {
@@ -100,6 +190,8 @@ const EmptySearchHeader = () => {
       handleChangeInput={handleChangeInput}
       viewMode={viewMode}
       handleValueChange={handleValueChange}
+      isTitleVisible={true}
+      isToggleVisible={true}
     />
   );
 };
