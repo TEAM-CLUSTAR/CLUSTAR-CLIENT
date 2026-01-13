cf640c7fac4deab8a86b40c07f799f8de9eb8ca6
jm8468 <jmtoyplay1113@gmail.com>
2026-01-13 15:35:38 +0900

    fix: FloatingLabel 컴포넌트의 가운데 미정렬 문제 해결

--- packages/cds-ui/src/components/floating-menu/floating-menu.css.ts
+++ packages/cds-ui/src/components/floating-menu/floating-menu.css.ts
@@ -6,6 +6,7 @@ export const container = style({
   display: 'inline-flex',
   height: '2.8rem',
   padding: '0.4rem 0.8rem',
+  alignItems: 'center',
   border: `1px solid ${themeVars.color.white}`,
   borderRadius: '8px',
   backgroundColor: themeVars.color.grey50,
