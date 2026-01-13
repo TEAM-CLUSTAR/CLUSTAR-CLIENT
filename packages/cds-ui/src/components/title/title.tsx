78573d097a58d9ce003ea81bde2696fcc1fc4698
jm8468 <jmtoyplay1113@gmail.com>
2026-01-14 07:25:42 +0900

    fix: 시맨틱 태그로 변경

--- packages/cds-ui/src/components/title/title.tsx
+++ packages/cds-ui/src/components/title/title.tsx
@@ -5,7 +5,7 @@ interface TitleProps {
 }
 
 const Title = ({ title }: TitleProps) => {
-  return <p className={styles.titleText}>{title}</p>;
+  return <h2 className={styles.titleText}>{title}</h2>;
 };
 
 export default Title;
