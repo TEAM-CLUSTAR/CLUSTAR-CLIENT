f94b69cdb91f0caeef4c0c6d465ce4f3cb51cbf6
twossu <nadao202@naver.com>
2026-01-15 02:19:31 +0900

    chore: 메모패이지 복구

--- apps/client/src/pages/memo/ui/memo-page.tsx
+++ apps/client/src/pages/memo/ui/memo-page.tsx
@@ -1,12 +1,9 @@
 import { Icon } from '@cds/icon';
 
-import InputTitle from '@features/memo/input-title';
-
 const MemoPage = () => {
   return (
-    <div style={{ backgroundColor: 'red', padding: '0 3rem' }}>
+    <div>
       <Icon name="ic_ai_gra" />
-      <InputTitle />
     </div>
   );
 };
