a2a63b29ec3e9b955c09df09ea79e65e5e26d91f
jogpfls <joerin4177@gmail.com>
2026-01-14 09:00:42 +0900

    feat: 라벨 색상에 대한 기본 색상 값 추가

--- packages/cds-ui/src/constants/label-color-map.ts
+++ packages/cds-ui/src/constants/label-color-map.ts
@@ -1,3 +1,5 @@
+import { themeVars } from '../styles';
+
 export type LabelTextType = '졸업 프로젝트' | '교양' | 'SOPT' | '레퍼런스';
 
 export type LabelColorType = 'blue' | 'green' | 'pink' | 'purple' | 'gray';
@@ -7,4 +9,15 @@ export const LABEL_COLOR_BY_TEXT: Record<LabelTextType, LabelColorType> = {
   교양: 'green',
   SOPT: 'pink',
   레퍼런스: 'purple',
+};
+
+export const PRIMARY_COLOR_VALUE_BY_LABEL_COLOR: Record<
+  LabelColorType,
+  string
+> = {
+  blue: themeVars.color.blue400,
+  green: themeVars.color.label05, // 프로젝트 토큰에 맞게
+  pink: themeVars.color.label07,
+  purple: themeVars.color.label01,
+  gray: themeVars.color.grey400,
 };
