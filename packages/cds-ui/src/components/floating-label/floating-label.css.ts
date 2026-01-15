1698c1fc1fe13d7a8dfe1bfe0ee78eac889afc2b
twossu <nadao202@naver.com>
2026-01-15 21:44:39 +0900

    fix: whiteSpace스타일 삭제

--- packages/cds-ui/src/components/floating-label/floating-label.css.ts
+++ packages/cds-ui/src/components/floating-label/floating-label.css.ts
@@ -13,6 +13,4 @@ export const container = style({
   backgroundColor: themeVars.color.grey50,
   ...themeVars.fontStyles.body_sb_14,
   color: themeVars.color.grey700,
-  whiteSpace: 'nowrap',
-  flexShrink: 0,
 });
--- packages/cds-ui/src/components/floating-menu/floating-menu.css.ts
+++ packages/cds-ui/src/components/floating-menu/floating-menu.css.ts
@@ -12,6 +12,4 @@ export const container = style({
   backgroundColor: themeVars.color.grey50,
   ...themeVars.fontStyles.body_sb_14,
   color: themeVars.color.grey700,
-  whiteSpace: 'nowrap',
-  flexShrink: 0,
 });
