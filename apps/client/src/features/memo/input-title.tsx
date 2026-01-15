cb585fe7955ea51632e2817afb126b59021c525c
twossu <nadao202@naver.com>
2026-01-16 00:00:11 +0900

    feat: 재 포커스시 현재 위치로 포커싱

--- apps/client/src/features/memo/input-title.tsx
+++ apps/client/src/features/memo/input-title.tsx
@@ -12,13 +12,19 @@ const InputTitle = ({ title, onChange }: InputTitleProps) => {
     e.target.scrollLeft = 0;
   };
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length <= 55) {
+      onChange(e);
+    }
+  };
+
   return (
     <input
       className={styles.container}
       placeholder="제목을 입력하세요."
       maxLength={55}
       value={title}
-      onChange={onChange}
+      onChange={handleChange}
       onBlur={handleBlur}
     />
   );
