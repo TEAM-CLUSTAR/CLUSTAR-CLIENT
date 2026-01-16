1e7c518972cb4a978140fbb9f283d60ea57aac2a
twossu <nadao202@naver.com>
2026-01-16 21:49:25 +0900

    chore: memo페이지 복구

--- apps/client/src/pages/memo/ui/memo-page.tsx
+++ apps/client/src/pages/memo/ui/memo-page.tsx
@@ -1,25 +1,11 @@
-// src/features/memo/ui/MemoEditor.tsx (예시)
-import { useState } from 'react';
-
-import { LabelTextType } from '@entities/memo/models/constant';
-
-import LabelSelect from '../../../features/memo/label-select/label-select'; // 경로에 맞춰 수정
-interface LabelItem {
-  id: string;
-  text: LabelTextType;
-}
+import { Icon } from '@cds/icon';
 
 const MemoPage = () => {
-  const [selectedLabels, setSelectedLabels] = useState<LabelItem[]>([]);
-
   return (
-    <main>
-      {/* 다른 메모 입력 요소들... */}
-      <LabelSelect
-        selectedItems={selectedLabels}
-        onSelect={setSelectedLabels}
-      />
-    </main>
+    <div>
+      <Icon name="ic_ai_gra" />
+    </div>
   );
 };
+
 export default MemoPage;
