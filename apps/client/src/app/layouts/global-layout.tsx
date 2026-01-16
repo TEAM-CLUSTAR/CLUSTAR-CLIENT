aed005a613d5e2cd6d7fd91fe3c58b8d7c1c7a5e
jogpfls <joerin4177@gmail.com>
2026-01-17 03:37:27 +0900

    fix: GlobalLayoutBoundary를 GlobalErrorBoundary로 수정

--- apps/client/src/app/layouts/global-layout.tsx
+++ apps/client/src/app/layouts/global-layout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router';
 
-import GlobalLayoutBoundary from '../providers/global-error-boundary';
+import GlobalErrorBoundary from '../providers/global-error-boundary';
 
 export default function GlobalLayout() {
   return (
-    <GlobalLayoutBoundary>
+    <GlobalErrorBoundary>
       <Outlet />
-    </GlobalLayoutBoundary>
+    </GlobalErrorBoundary>
   );
 }
