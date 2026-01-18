b7d3f3b4455fc96fa15f5542c2d95998b5b540c7
jyeon03 <bjlyeon2003@naver.com>
2026-01-18 16:49:25 +0900

    fix: ai-prompt ui 폴더 구조 변경

---
+++ apps/client/src/features/ai-prompt/ui/index.ts
@@ -0,0 +1,6 @@
+export { default as AiMessage } from './ai-message/ai-message';
+export { default as PromptHeader } from './prompt-header/prompt-header';
+export { default as PromptInput } from './prompt-input/prompt-input';
+export { default as PromptMessagesList } from './prompt-messages-list/prompt-messages-list';
+export { default as PromptOption } from './prompt-option/prompt-option';
+export { default as SelectedMemosList } from './selected-memo-section/selected-memo-section';
---
+++ apps/client/src/features/ai-prompt/ui/prompt-header/prompt-header.css.ts
@@ -0,0 +1,25 @@
+import { style } from '@vanilla-extract/css';
+
+import { themeVars } from '@cds/ui';
+
+export const header = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  padding: '2.2rem 0 2.2rem 0',
+  margin: '0 -2.4rem',
+  paddingLeft: '2.4rem',
+  paddingRight: '2.4rem',
+  borderBottom: `1px solid ${themeVars.color.grey300}`,
+});
+
+export const titleContainer = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.4rem',
+});
+
+export const title = style({
+  ...themeVars.fontStyles.title_sb_18,
+  color: themeVars.color.blue500,
+});
