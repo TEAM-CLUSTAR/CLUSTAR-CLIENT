9654e5be1c7a7acdb8c62cb556e3bf4600db6393
jyeon03 <bjlyeon2003@naver.com>
2026-01-18 16:49:51 +0900

    fix: 타입 & 로직 코드 분리

---
+++ apps/client/src/features/ai-prompt/model/index.ts
@@ -0,0 +1,8 @@
+export type {
+  Message,
+  MessageType,
+  SelectedMemo,
+  UseAiPromptProps,
+} from './types';
+export { useAiPrompt } from './use-ai-prompt';
+export { useMessagesScroll } from './use-ai-prompt';
