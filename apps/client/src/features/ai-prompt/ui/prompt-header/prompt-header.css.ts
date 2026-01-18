672263c7e97d83819bbb6e7d6e75cc1337e0bb3d
jyeon03 <bjlyeon2003@naver.com>
2026-01-18 23:17:23 +0900

    fix: 패딩값 수정

--- apps/client/src/features/ai-prompt/ui/prompt-header/prompt-header.css.ts
+++ apps/client/src/features/ai-prompt/ui/prompt-header/prompt-header.css.ts
@@ -6,10 +6,8 @@ export const header = style({
   display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  padding: '2.2rem 0 2.2rem 0',
+  padding: '2.2rem 2.4rem',
   margin: '0 -2.4rem',
-  paddingLeft: '2.4rem',
-  paddingRight: '2.4rem',
   borderBottom: `1px solid ${themeVars.color.grey300}`,
 });
 
