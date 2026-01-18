4cc078a28b035d18b0ddf2cd7ea8ba70eb837923
jm8468 <jmtoyplay1113@gmail.com>
2026-01-18 22:51:56 +0900

    fix: 폴더명 수정 및 index.ts 수정

--- apps/client/src/entities/memo/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Tab } from './tab/tab';
-export { default as TabList } from './tab-list/tab-list';
-export { default as Toolbar } from './toolbar/toolbar';
-export { default as TreeMemo } from './tree-memo/tree-memo';
--- apps/client/src/features/tree-memo-list/ui/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as TreeMemoList } from './tree-memo-list';
