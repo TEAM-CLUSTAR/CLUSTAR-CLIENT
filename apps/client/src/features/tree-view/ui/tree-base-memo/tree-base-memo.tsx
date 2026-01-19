2fa27df67b98179708c44110e2dcdaa70834a6aa
jm8468 <jmtoyplay1113@gmail.com>
2026-01-19 21:23:11 +0900

    feat: 트리뷰의 최상위 노드 컴포넌트 구현

---
+++ apps/client/src/features/tree-view/ui/tree-base-memo/tree-base-memo.tsx
@@ -0,0 +1,11 @@
+import * as styles from './tree-base-memo.css';
+
+const TreeBaseMemo = () => {
+  return (
+    <div className={styles.container}>
+      <span className={styles.text}>전체 메모</span>
+    </div>
+  );
+};
+
+export default TreeBaseMemo;
