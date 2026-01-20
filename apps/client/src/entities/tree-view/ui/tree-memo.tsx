6b5104d83ce219317e572e287bd947382448cda8
jm8468 <jmtoyplay1113@gmail.com>
2026-01-21 04:17:38 +0900

    fix: 현재 불필요한 DetailModal 컴포넌트 제거

--- apps/client/src/entities/tree-view/ui/tree-memo.tsx
+++ apps/client/src/entities/tree-view/ui/tree-memo.tsx
@@ -1,5 +1,3 @@
-import { DetailModal } from '@cds/ui';
-
 import { LABEL_COLOR_BY_TEXT } from '@shared/constants/label-match';
 import { LabelTextType } from '@shared/types/label-type';
 import { StructureMemoTypes } from '@shared/types/memo-info-type';
