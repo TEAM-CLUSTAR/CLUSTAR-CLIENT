0c6acd7e904f2fe2477170cb5ab13d63d6228c7c
jogpfls <joerin4177@gmail.com>
2026-01-21 03:44:38 +0900

    feat: 무한 스크롤을 지원하기 위해 getAllMemo 함수 수정 및 MemoCursor 타입 추가

--- apps/client/src/pages/all-memo/api/queries.ts
+++ apps/client/src/pages/all-memo/api/queries.ts
@@ -1,6 +1,7 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
 import { api } from '@shared/api/instance';
+import { components } from '@shared/types/schema';
 
 import { type MockMemo } from '@widgets/memo-list/types/memo';
 import { mapApiMemoToMockMemo } from '@widgets/memo-list/types/memo';
@@ -9,22 +10,95 @@ import { ALL_MEMO_END_POIINT } from './end-point';
 import { ALL_MEMO_KEY } from './query-key';
 import { type AllMemoResponse } from './type';
 
-export const getAllMemo = async (
+type MemoCursor =
+  | {
+      cursorCreatedAt?: string;
+      cursorMemoId?: number;
+    }
+  | undefined;
+
+type MemoDashboardResponse = components['schemas']['MemoDashboardResponse'];
+
+/**
+ * API 응답에서 memos 배열을 추출하는 헬퍼 함수
+ * getAllMemo에서 response.data를 반환하므로, AllMemoResponse는 ApiResponseMemoListDashboardResponse 형태
+ * 즉, { code, msg, data: { totalCount, memos } } 형태
+ */
+const getMemosFromResponse = (
+  response: AllMemoResponse,
+): MemoDashboardResponse[] => {
+  return response.data?.memos ?? [];
+};
+
+const getAllMemo = async (
   labelIds?: number[],
+  cursor?: MemoCursor,
+  size = 20,
 ): Promise<AllMemoResponse> => {
   const response = await api.get<AllMemoResponse>(ALL_MEMO_END_POIINT.GET, {
-    params: { labelIds },
+    params: {
+      labelIds,
+      cursorCreatedAt: cursor?.cursorCreatedAt,
+      cursorMemoId: cursor?.cursorMemoId,
+      size,
+    },
   });
   return response.data;
 };
 
-export const useGetAllMemo = (labelIds?: number[]) => {
-  return useQuery<AllMemoResponse, Error, MockMemo[]>({
+const getMemoTotalCount = async (
+  labelIds?: number[],
+): Promise<number | undefined> => {
+  const response = await api.get<AllMemoResponse>(ALL_MEMO_END_POIINT.GET, {
+    params: {
+      labelIds,
+      size: 1,
+    },
+  });
+  return response.data.data?.totalCount;
+};
+
+export const useGetMemoTotalCount = (labelIds?: number[]) => {
+  return useQuery({
+    queryKey: [
+      ...ALL_MEMO_KEY.ALL,
+      'totalCount',
+      ...(labelIds ? [{ labelIds }] : []),
+    ],
+    queryFn: () => getMemoTotalCount(labelIds),
+  });
+};
+
+export const useGetAllMemo = (labelIds?: number[], size = 20) => {
+  return useInfiniteQuery<
+    AllMemoResponse,
+    Error,
+    MockMemo[],
+    ReturnType<typeof ALL_MEMO_KEY.GET>,
+    MemoCursor
+  >({
     queryKey: ALL_MEMO_KEY.GET(labelIds),
-    queryFn: () => getAllMemo(labelIds),
-    select: (res) => {
-      const apiMemos = res.data?.memos ?? [];
-      return apiMemos.map(mapApiMemoToMockMemo);
+    queryFn: ({ pageParam }) => getAllMemo(labelIds, pageParam, size),
+    getNextPageParam: (lastPage) => {
+      const memos = getMemosFromResponse(lastPage);
+      const last = memos[memos.length - 1];
+
+      if (!last) return undefined;
+
+      return {
+        cursorCreatedAt: last.createdAt,
+        cursorMemoId: last.memoId,
+      };
+    },
+    initialPageParam: undefined,
+    select: (data) => {
+      const flatApiMemos: MemoDashboardResponse[] = data.pages.flatMap((p) =>
+        getMemosFromResponse(p),
+      );
+      const result: MockMemo[] = flatApiMemos.map(
+        (memo: MemoDashboardResponse) => mapApiMemoToMockMemo(memo),
+      );
+      return result;
     },
   });
 };
