872ef46741c661aa3746987c96fb64b8fbfe2356
jm8468 <jmtoyplay1113@gmail.com>
2026-04-02 22:57:10 +0900

    feat: modalRoot 컴포넌트 및 컴파운드 패턴 구현

--- packages/cds-ui/src/components/modal/modal.tsx
+++ packages/cds-ui/src/components/modal/modal.tsx
@@ -1,73 +1,61 @@
-import { ReactNode } from 'react';
-import * as Dialog from '@radix-ui/react-dialog';
+import { ReactNode, useCallback, useMemo, useState } from 'react';
 
-import { ThemeProvider } from '../../providers/theme-provider';
+import Close from './components/close';
+import Content from './components/content';
+import Trigger from './components/trigger';
+import useEscapeKey from './hooks/use-escape-key';
+import useScrollLock from './hooks/use-scroll-lock';
+import { ModalContext } from './modal-context';
 
-import * as styles from './modal.css';
-
-interface ModalRootProps {
-  defaultOpen?: boolean;
+interface ModalProps {
+  children: ReactNode;
   open?: boolean;
   onOpenChange?: (open: boolean) => void;
-  children: ReactNode;
-}
-interface ModalTriggerProps {
-  children: ReactNode;
-}
-interface ModalCloseProps {
-  children: ReactNode;
-}
-interface ModalContentProps {
-  children: ReactNode;
+  defaultOpen?: boolean;
 }
 
 const ModalRoot = ({
-  defaultOpen,
-  open,
-  onOpenChange,
   children,
-}: ModalRootProps) => {
-  return (
-    <Dialog.Root
-      open={open}
-      defaultOpen={defaultOpen}
-      onOpenChange={onOpenChange}
-    >
-      {children}
-    </Dialog.Root>
+  open, // 부모가 넘기는 모달 열림 상태, controlled 모드
+  onOpenChange, // 모달 상태가 바뀔 때 부모한테 알리는 콜백
+  defaultOpen = false, // uncontrolled 모드의 초기값
+}: ModalProps) => {
+  const isControlled = open !== undefined;
+  const [internalOpen, setInternalOpen] = useState(defaultOpen); // uncontrolled 모드에서 사용하는 상태값
+
+  const isOpen = isControlled ? open : internalOpen; // Context로 보내는 최종 열림 상태. controlled -> open, uncontrolled -> internalopen
+
+  // 모달을 여는 함수
+  const onOpen = useCallback(() => {
+    if (!isControlled) setInternalOpen(true);
+    onOpenChange?.(true);
+  }, [isControlled, onOpenChange]);
+
+  // 모달을 닫는 함수
+  const onClose = useCallback(() => {
+    if (!isControlled) setInternalOpen(false);
+    onOpenChange?.(false);
+  }, [isControlled, onOpenChange]);
+
+  useScrollLock(isOpen);
+  useEscapeKey({ isOpen, onClose });
+
+  const contextValue = useMemo(
+    () => ({ isOpen, onOpen, onClose }),
+    [isOpen, onOpen, onClose],
   );
-};
 
-const ModalTrigger = ({ children }: ModalTriggerProps) => {
-  return <Dialog.Trigger asChild>{children}</Dialog.Trigger>;
-};
-
-const ModalClose = ({ children }: ModalCloseProps) => {
-  return <Dialog.Close asChild>{children}</Dialog.Close>;
-};
-
-const ModalContent = ({ children }: ModalContentProps) => {
   return (
-    <Dialog.Portal>
-      <ThemeProvider>
-        <Dialog.Overlay className={styles.overlay} />
-        <Dialog.Content
-          onOpenAutoFocus={(event) => {
-            event.preventDefault();
-          }}
-          className={styles.content}
-        >
-          {children}
-        </Dialog.Content>
-      </ThemeProvider>
-    </Dialog.Portal>
+    <ModalContext.Provider value={contextValue}>
+      {children}
+    </ModalContext.Provider>
   );
 };
 
 const Modal = Object.assign(ModalRoot, {
-  Trigger: ModalTrigger,
-  Close: ModalClose,
-  Content: ModalContent,
+  Trigger,
+  Content,
+  Close,
 });
 
 export default Modal;
