0a6aef1df1dd8ac8c58e258728067cad73bec792
jm8468 <jmtoyplay1113@gmail.com>
2026-04-02 22:55:44 +0900

    feat: 모달 esc 상호작용 커스텀훅 추가

---
+++ packages/cds-ui/src/components/modal/hooks/use-escape-key.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+interface useEscapeKeyTypes {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const useEscapeKey = ({ isOpen, onClose }: useEscapeKeyTypes) => {
+  useEffect(() => {
+    if (!isOpen) return;
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+};
+
+export default useEscapeKey;
