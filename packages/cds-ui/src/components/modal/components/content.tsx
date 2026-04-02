0cba7d54bcec87872c61642d4f446799fe245214
jm8468 <jmtoyplay1113@gmail.com>
2026-04-02 22:52:12 +0900

    feat: modal content 컴포넌트 구현

---
+++ packages/cds-ui/src/components/modal/components/content.tsx
@@ -0,0 +1,57 @@
+import { ReactNode, useEffect, useState } from 'react';
+
+import { ThemeProvider } from '../../../providers/theme-provider';
+import { useFocusTrap } from '../hooks/use-focus-trap';
+import { useModalContext } from '../modal-context';
+import Overlay from './overlay';
+import Portal from './portal';
+
+import * as styles from '../modal.css';
+
+export interface ContentProps {
+  children: ReactNode;
+  className?: string;
+  ariaLabel?: string;
+}
+
+const Content = ({ children, ariaLabel = 'modal' }: ContentProps) => {
+  const { isOpen } = useModalContext();
+
+  const [mounted, setMounted] = useState(false);
+  const ref = useFocusTrap(isOpen && mounted);
+  useEffect(() => {
+    if (isOpen) {
+      setMounted(true);
+    }
+  }, [isOpen]);
+
+  const handleAnimationEnd = () => {
+    if (!isOpen) setMounted(false);
+  };
+
+  if (!mounted) return null;
+
+  return (
+    <Portal>
+      <ThemeProvider>
+        <Overlay
+          className={styles.overlay}
+          dataState={isOpen ? 'open' : 'closed'}
+        />
+        <div
+          ref={ref}
+          role="dialog"
+          aria-modal="true"
+          aria-label={ariaLabel}
+          className={styles.content}
+          data-state={isOpen ? 'open' : 'closed'}
+          onAnimationEnd={handleAnimationEnd}
+        >
+          {children}
+        </div>
+      </ThemeProvider>
+    </Portal>
+  );
+};
+
+export default Content;
