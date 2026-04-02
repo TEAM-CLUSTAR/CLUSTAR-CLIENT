cc08155e3040e8e2704e4f483ab475198b0b6a11
jm8468 <jmtoyplay1113@gmail.com>
2026-04-02 22:52:57 +0900

    feat: modal overlay 컴포넌트 구현

---
+++ packages/cds-ui/src/components/modal/components/overlay.tsx
@@ -0,0 +1,24 @@
+import { useModalContext } from '../modal-context';
+
+export interface OverlayProps {
+  className?: string;
+  dataState?: 'open' | 'closed';
+}
+
+const Overlay = ({ className, dataState }: OverlayProps) => {
+  const { isOpen, onClose } = useModalContext();
+
+  if (!isOpen) return null;
+
+  return (
+    <div
+      role="presentation"
+      aria-hidden="true"
+      className={className}
+      data-state={dataState}
+      onClick={onClose}
+    />
+  );
+};
+
+export default Overlay;
