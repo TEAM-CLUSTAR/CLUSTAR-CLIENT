6021c44b8fc068b6a7e6bbfb739c76a6dc2e6f26
jm8468 <jmtoyplay1113@gmail.com>
2026-05-19 18:50:59 +0900

    fix: UseModalContext의 null 체크 로직 단순화

--- packages/cds-ui/src/components/modal/modal-context.ts
+++ packages/cds-ui/src/components/modal/modal-context.ts
@@ -11,7 +11,7 @@ export const ModalContext = createContext<ModalContextTypes | null>(null);
 export const useModalContext = (): ModalContextTypes => {
   const context = useContext(ModalContext);
 
-  if (context === null) {
+  if (!context) {
     throw new Error(
       'useModalContext는 Modal 컴포넌트 내부에서만 사용할 수 있습니다.',
     );
