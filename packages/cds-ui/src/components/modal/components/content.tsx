43a11c3eccc932c6eedfbd7e52a346e41d417ab8
jm8468 <jmtoyplay1113@gmail.com>
2026-06-20 22:18:08 +0900

    fix: Overlay, Portal 컴포넌트를 Conent 컴포넌트에 통합

--- packages/cds-ui/src/components/modal/components/content.tsx
+++ packages/cds-ui/src/components/modal/components/content.tsx
@@ -4,26 +4,21 @@ import { createPortal } from 'react-dom';
 import { ThemeProvider } from '../../../providers/theme-provider';
 import useFocusTrap from '../hooks/use-focus-trap';
 import { useModalContext } from '../modal-context';
-import Overlay from './overlay';
 
 import * as styles from '../modal.css';
 
-interface PortalProps {
-  children: ReactNode;
-  container?: HTMLElement;
-}
 export interface ContentProps {
+  className?: string;
   children: ReactNode;
   ariaLabel?: string;
 }
 
-const Portal = ({ children, container }: PortalProps) => {
-  if (typeof document === 'undefined') return null;
-  return createPortal(children, container ?? document.body);
-};
-
-const Content = ({ children, ariaLabel = 'modal' }: ContentProps) => {
-  const { isOpen } = useModalContext();
+const Content = ({
+  className,
+  children,
+  ariaLabel = 'modal',
+}: ContentProps) => {
+  const { isOpen, onClose } = useModalContext();
   const [mounted, setMounted] = useState(false);
   const focusTrapRef = useFocusTrap(isOpen && mounted);
 
@@ -36,26 +31,31 @@ const Content = ({ children, ariaLabel = 'modal' }: ContentProps) => {
   };
 
   if (!mounted) return null;
-  return (
-    <Portal>
-      <ThemeProvider>
-        <Overlay
-          className={styles.overlay}
-          dataState={isOpen ? 'open' : 'closed'}
-        />
-        <div
-          ref={focusTrapRef}
-          role="dialog"
-          aria-modal="true"
-          aria-label={ariaLabel}
-          className={styles.content}
-          data-state={isOpen ? 'open' : 'closed'}
-          onAnimationEnd={handleAnimationEnd}
-        >
-          {children}
-        </div>
-      </ThemeProvider>
-    </Portal>
+  return createPortal(
+    <ThemeProvider>
+      {/* Overlay */}
+      <div
+        role="presentation"
+        aria-hidden="true"
+        className={styles.overlay}
+        data-state={isOpen ? 'open' : 'closed'}
+        onAnimationEnd={handleAnimationEnd}
+        onClick={onClose}
+      />
+      {/* Content */}
+      <div
+        ref={focusTrapRef}
+        role="dialog"
+        aria-modal="true"
+        aria-label={ariaLabel}
+        className={[styles.content, className].filter(Boolean).join(' ')}
+        data-state={isOpen ? 'open' : 'closed'}
+        onAnimationEnd={handleAnimationEnd}
+      >
+        {children}
+      </div>
+    </ThemeProvider>,
+    document.body,
   );
 };
 
