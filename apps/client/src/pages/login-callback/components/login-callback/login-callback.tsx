88a324fb3fd616ce9845e8d5acefc36e56935597
jogpfls <joerin4177@gmail.com>
2026-06-22 23:37:04 +0900

    fix: 구글 코드 교환을 위한 API 엔드포인트 경로 수정

--- apps/client/src/pages/login-callback/components/login-callback/login-callback.tsx
+++ apps/client/src/pages/login-callback/components/login-callback/login-callback.tsx
@@ -3,7 +3,7 @@ import { PATH } from '@router/path';
 import { useLocation, useNavigate } from 'react-router';
 
 import { LoadingPage } from '@pages/loading';
-import { exchangeGoogleCode } from '@pages/login/apis/exchange-google-code';
+import { exchangeGoogleCode } from '@pages/login-callback/apis/exchange-google-code';
 
 import { setAccessToken } from '@shared/utils/token-storage';
 
