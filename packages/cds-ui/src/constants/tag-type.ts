fe7fc59d9c31236c9d22e28f9242d7df5f2361a1
twossu <nadao202@naver.com>
2026-06-25 07:06:20 +0900

    feat: 태그 서브 컬러 10종 추가 및 색상 매핑 정리

--- packages/cds-ui/src/constants/tag-type.ts
+++ packages/cds-ui/src/constants/tag-type.ts
@@ -3,23 +3,35 @@ import { themeVars } from '../styles';
 export type TagTextType = 'AI 결과물' | '태그 없음';
 
 export type TagColorType =
+  | 'pink'
+  | 'red'
+  | 'orange'
+  | 'yellow'
+  | 'green'
+  | 'cyan'
+  | 'lightBlue'
   | 'blue'
   | 'purple'
-  | 'green'
-  | 'pink'
-  | 'aiBlue'
+  | 'magenta'
+  | 'gradient'
   | 'grey';
 
 export const TAG_COLOR_BY_TEXT: Record<TagTextType, TagColorType> = {
-  'AI 결과물': 'aiBlue',
+  'AI 결과물': 'gradient',
   '태그 없음': 'grey',
 };
 
 export const PRIMARY_COLOR_VALUE_BY_TAG_COLOR: Record<TagColorType, string> = {
-  blue: themeVars.color.sub04,
-  purple: themeVars.color.sub02,
-  green: themeVars.color.sub06,
-  pink: themeVars.color.sub08,
-  aiBlue: themeVars.color.blue50,
+  pink: themeVars.color.sub_pink_100,
+  red: themeVars.color.sub_red_100,
+  orange: themeVars.color.sub_orange_100,
+  yellow: themeVars.color.sub_yellow_100,
+  green: themeVars.color.sub_green_100,
+  cyan: themeVars.color.sub_cyan_100,
+  lightBlue: themeVars.color.sub_lightBlue_100,
+  blue: themeVars.color.sub_blue_100,
+  purple: themeVars.color.sub_purple_100,
+  magenta: themeVars.color.sub_magenta_100,
+  gradient: themeVars.color.blue50,
   grey: themeVars.color.grey200,
 };
