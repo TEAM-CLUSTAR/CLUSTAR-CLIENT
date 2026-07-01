69bf38217b027bcafbde526307f34c9e8f9f5c6a
jm8468 <jmtoyplay1113@gmail.com>
2026-07-02 00:45:12 +0900

    feat: FilterListItem 컴포넌트 구현

---
+++ apps/client/src/pages/memo/components/filter-list-item/filter-list-item.tsx
@@ -0,0 +1,38 @@
+import { Icon } from '@cds/icon';
+
+import * as styles from './filter-list-item.css';
+
+type RelationTypes = 'parent' | 'child' | 'descendant';
+
+interface FilterListItemProps {
+  relation: RelationTypes;
+  tag: string;
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+}
+
+const FilterListItem = ({
+  relation,
+  tag,
+  checked,
+  onChange,
+}: FilterListItemProps) => {
+  return (
+    <label className={styles.container({ relation })}>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        checked={checked}
+        onChange={(e) => onChange(e.target.checked)}
+      />
+      <Icon
+        name={checked ? 'ic_checkbox_on' : 'ic_checkbox_off'}
+        className={styles.icon}
+        size={24}
+      />
+      <span className={styles.tag}>{tag}</span>
+    </label>
+  );
+};
+
+export default FilterListItem;
