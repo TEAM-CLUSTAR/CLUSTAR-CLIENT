db712f3e455b789c265c1ad97a3e20ce0de1dae8
jogpfls <joerin4177@gmail.com>
2026-07-02 21:14:46 +0900

    fix: 로그인 링크를 a 태그에서 Link 컴포넌트로 변경

--- apps/client/src/shared/components/layouts/auth-layout/auth-layout.tsx
+++ apps/client/src/shared/components/layouts/auth-layout/auth-layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 import { PATH } from '@router/path';
+import { Link } from 'react-router';
 
 import { Icon } from '@cds/icon';
 
@@ -14,9 +15,9 @@ const AuthLayout = ({ children }: { children: ReactNode }) => {
           <Icon name="ic_logo_symbol" size={42.67} />
           <Icon name="ic_logo_type" width={106.5} height={13.84} />
         </div>
-        <a href={PATH.LOGIN} className={styles.loginLink}>
+        <Link to={PATH.LOGIN} className={styles.loginLink}>
           로그인
-        </a>
+        </Link>
       </header>
       {children}
     </>
