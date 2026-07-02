c3cfbe0092e7c905855ffc82fbdb0d73b2e74683
jm8468 <jmtoyplay1113@gmail.com>
2026-07-03 03:10:30 +0900

    refactor: index.ts 개선
    외부로 export 되는 컴포넌트만 index.ts에 존재하도록 수정

--- apps/client/src/shared/components/memo-list-view/components/tree-view/components/index.ts
+++ apps/client/src/shared/components/memo-list-view/components/tree-view/components/index.ts
@@ -1,5 +1,3 @@
 export { default as CustomEdge } from './custom-edge';
 export { default as TreeBaseMemoNode } from './tree-base-memo-node/tree-base-memo-node';
-export { default as TreeCustomHandle } from './tree-custom-handle/tree-custom-handle';
-export { default as TreeMemo } from './tree-memo/tree-memo';
 export { default as TreeMemoListNode } from './tree-memo-list-node/tree-memo-list-node';
