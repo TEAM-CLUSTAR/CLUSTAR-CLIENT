3ff7215f3e5abdf9a2d1bb9d72666b0d58e5556b
jm8468 <jmtoyplay1113@gmail.com>
2026-07-03 16:45:29 +0900

    refactor: 불필요한 폴더 깊이 축소
    - modal/confirm-modal -> confirm-modal로 폴더 깊이 변경

--- apps/client/src/shared/components/ai-prompt/ai-prompt.tsx
+++ apps/client/src/shared/components/ai-prompt/ai-prompt.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import ConfirmModal from '../modals/confirm-modal/confirm-modal';
+import ConfirmModal from '../confirm-modal/confirm-modal';
 import AiPromptHeader from './components/prompt-header/prompt-header';
 import PromptInput from './components/prompt-input/prompt-input';
 import AiMessagesList from './components/prompt-messages-list/prompt-messages-list';
