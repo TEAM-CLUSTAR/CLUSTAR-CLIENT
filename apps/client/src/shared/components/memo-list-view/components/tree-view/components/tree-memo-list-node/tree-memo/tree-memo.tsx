a2353ef8e12a1f5398b72e8a390b4b49c16026e2
jm8468 <jmtoyplay1113@gmail.com>
2026-07-03 17:25:54 +0900

    fix: 객체 리터럴 중복 생성 방지

--- apps/client/src/shared/components/memo-list-view/components/tree-view/components/tree-memo-list-node/tree-memo/tree-memo.tsx
+++ apps/client/src/shared/components/memo-list-view/components/tree-view/components/tree-memo-list-node/tree-memo/tree-memo.tsx
@@ -5,7 +5,7 @@ import { LabelTextType } from '@shared/types/label-type';
 import { StructureMemoTypes } from '@shared/types/memo-info-type';
 
 import DetailModal from '../detail-modal/detail-modal';
-import { useDetailMemo } from './apis/queries';
+import { SelectedMemoTypes, useDetailMemo } from './apis/queries';
 import useSingleAndDoubleClick from './hooks/use-single-and-double-click';
 
 import * as styles from './tree-memo.css';
@@ -14,24 +14,26 @@ interface TreeMemoProps {
   memo: StructureMemoTypes;
 }
 
+const DEFAULT_MEMO_DETAIL: SelectedMemoTypes = {
+  memoId: 0,
+  title: '',
+  content: '',
+  images: [],
+  files: [],
+  labelList: [],
+  createdAt: '',
+  isAiGenerated: false,
+  sourceMemoTitleList: [],
+};
+
 const TreeMemo = ({ memo }: TreeMemoProps) => {
   const { memoId, title, content, labelList } = memo;
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // 모달이 열릴 때만 API 호출
-  const {
-    data: memoDetail = {
-      memoId: 0,
-      title: '',
-      content: '',
-      images: [],
-      files: [],
-      labelList: [],
-      createdAt: '',
-      isAiGenerated: false,
-      sourceMemoTitleList: [],
-    },
-  } = useDetailMemo({ memoId, enabled: isModalOpen });
+  const { data: memoDetail = DEFAULT_MEMO_DETAIL } = useDetailMemo({
+    memoId,
+    enabled: isModalOpen,
+  });
   const labelName = labelList[0]?.name ?? '라벨없음';
   const labelColor = LABEL_COLOR_BY_TEXT[labelName as LabelTextType];
 
