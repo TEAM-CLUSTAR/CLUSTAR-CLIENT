c892ad80bd00e518778886044b8fab388e3f219d
jyeon03 <bjlyeon2003@naver.com>
2026-07-30 00:01:22 +0900

    fix: 리디자인에 맞춰 기존 Search 컴포넌트 제거

--- packages/cds-ui/src/components/index.ts
+++ packages/cds-ui/src/components/index.ts
@@ -6,7 +6,6 @@ export { default as Label } from './label/label';
 export { default as LabelList } from './label-list/label-list';
 export { default as Modal } from './modal/modal';
 export { default as PageTitle } from './page-title/page-title';
-export { default as Search } from './search/search';
 export { default as SidebarIcon } from './sidebar-icon/sidebar-icon';
 export { default as SidebarPannel } from './sidebar-pannel/sidebar-pannel';
 export { default as SideBarProfile } from './sidebar-profile/sidebar-profile';
