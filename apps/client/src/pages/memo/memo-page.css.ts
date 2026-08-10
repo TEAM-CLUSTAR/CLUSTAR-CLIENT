473c0d137d7ea3343bdb243092d446f25dde6fe4
jogpfls <joerin4177@gmail.com>
2026-08-11 05:38:49 +0900

    chore: memo-page의 컨테이너 스타일 수정 및 배경 제거

--- apps/client/src/pages/memo/memo-page.css.ts
+++ apps/client/src/pages/memo/memo-page.css.ts
@@ -1,10 +1,8 @@
 import { style } from '@vanilla-extract/css';
 
-import { themeVars } from '@cds/ui';
-
 export const container = style({
-  ...themeVars.fontStyles.display_sb_36,
-  backgroundImage: themeVars.color.gradient02,
-  color: themeVars.color.grey700,
-  width: '30rem',
+  width: '100%',
+  paddingInline: '4rem',
+  display: 'flex',
+  justifyContent: 'center',
 });
