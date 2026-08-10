0595db98036489ccea42f1a94d70af92ef8d9285
jogpfls <joerin4177@gmail.com>
2026-08-11 05:38:38 +0900

    chore: memoListContainer의 최대 및 최소 너비 계산 수정

--- apps/client/src/pages/memo/components/memo-list/memo-list.css.ts
+++ apps/client/src/pages/memo/components/memo-list/memo-list.css.ts
@@ -1,12 +1,14 @@
 import { style } from '@vanilla-extract/css';
 
 const CARD_MIN_WIDTH = '34rem';
+const CARD_MAX_WIDTH = '38rem';
 const GAP = '2rem';
 
 export const memoListContainer = style({
   display: 'grid',
   gridTemplateColumns: `repeat(auto-fill, minmax(${CARD_MIN_WIDTH}, max-content))`,
-  minWidth: `calc(${CARD_MIN_WIDTH} * 2 + ${GAP})`,
   justifyContent: 'safe center',
   gap: GAP,
+  maxWidth: `calc(${CARD_MAX_WIDTH} * 3 + ${GAP} * 2)`,
+  minWidth: `calc(${CARD_MIN_WIDTH} * 2 + ${GAP} )`,
 });
