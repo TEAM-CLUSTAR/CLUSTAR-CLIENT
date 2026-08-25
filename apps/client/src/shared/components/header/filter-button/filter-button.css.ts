536eb2aaae0a5c5c26f81ea5f4838e675ddaf440
twossu <nadao202@naver.com>
2026-08-25 22:43:40 +0900

    feat: 필터 버튼 컴포넌트 구현

---
+++ apps/client/src/shared/components/header/filter-button/filter-button.css.ts
@@ -0,0 +1,40 @@
+import { recipe } from '@vanilla-extract/recipes';
+
+import { themeVars } from '@cds/ui';
+
+export const filterButton = recipe({
+  base: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '0.4rem',
+    padding: '0.4rem 1.2rem 0.4rem 0.8rem',
+    borderRadius: '8px',
+  },
+  variants: {
+    isActive: {
+      true: {
+        backgroundColor: themeVars.color.blue50,
+        border: `1px solid ${themeVars.color.blue500}`,
+      },
+      false: {
+        border: `1px solid ${themeVars.color.grey400}`,
+      },
+    },
+  },
+});
+
+export const label = recipe({
+  base: {
+    ...themeVars.fontStyles.body_m_16,
+  },
+  variants: {
+    isActive: {
+      true: {
+        color: themeVars.color.blue500,
+      },
+      false: {
+        color: themeVars.color.grey700,
+      },
+    },
+  },
+});
---
+++ apps/client/src/shared/components/header/filter-button/filter-button.tsx
@@ -0,0 +1,28 @@
+import { Icon } from '@cds/icon';
+
+import * as styles from './filter-button.css';
+
+interface FilterButtonProps {
+  isActive: boolean;
+  onClick: () => void;
+}
+
+const FilterButton = ({ isActive, onClick }: FilterButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={styles.filterButton({ isActive })}
+      onClick={onClick}
+      aria-label="필터링"
+    >
+      <Icon
+        name="ic_filter"
+        size={28}
+        color={isActive ? 'blue500' : 'grey700'}
+      />
+      <p className={styles.label({ isActive })}>필터링</p>
+    </button>
+  );
+};
+
+export default FilterButton;
