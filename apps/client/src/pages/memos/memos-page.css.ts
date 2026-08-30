2e46be14afee03311e10253480f98d58f78a2d6a
twossu <nadao202@naver.com>
2026-08-30 19:46:26 +0900

    feat: memos-page 헤더 고정 스타일 적용

--- apps/client/src/pages/memos/memos-page.css.ts
+++ apps/client/src/pages/memos/memos-page.css.ts
@@ -1,5 +1,7 @@
 import { style } from '@vanilla-extract/css';
 
+import { themeVars } from '@cds/ui';
+
 const HEADER_CONTAINER = 'header';
 const HEADER_MAX_WIDTH = '1180px';
 const HEADER_MIN_WIDTH = '800px';
@@ -9,16 +11,16 @@ export const container = style({
   height: '100%',
   display: 'flex',
   flexDirection: 'column',
+  containerType: 'inline-size',
+  containerName: HEADER_CONTAINER,
+  overflow: 'hidden',
+  scrollbarGutter: 'stable',
 });
 
 export const headerSection = style({
   width: '100%',
   paddingInline: '4rem',
   flexShrink: 0,
-  containerType: 'inline-size',
-  containerName: HEADER_CONTAINER,
-  overflow: 'hidden',
-  scrollbarGutter: 'stable',
 });
 
 export const headerContainer = style({
@@ -26,6 +28,9 @@ export const headerContainer = style({
   minWidth: HEADER_MIN_WIDTH,
   maxWidth: HEADER_MIN_WIDTH,
   marginInline: 'auto',
+  position: 'sticky',
+  top: 0,
+  backgroundColor: themeVars.color.grey50,
 
   '@container': {
     [`${HEADER_CONTAINER} (min-width: 1060px)`]: {
