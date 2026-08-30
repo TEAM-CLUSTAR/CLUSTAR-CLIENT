fed067e1a08942fa1fbe00b1a009a5dd1fbb30d1
jm8468 <jmtoyplay1113@gmail.com>
2026-08-30 16:40:18 +0900

    chore: 삭제 TODO 추가

--- apps/client/src/pages/memos/components/memo-detail/memo-detail.tsx
+++ apps/client/src/pages/memos/components/memo-detail/memo-detail.tsx
@@ -1,5 +1,5 @@
 import MemoEditor from '@shared/components/memo-editor/memo-editor';
-
+// @TODO: 이 파일은 삭제하고 내부 컴포넌트는 사용처로 옮겨요
 interface MemoDetailProps {
   memoId: number;
   /** 메모 삭제 성공 시 호출돼요. 상위(탭)에서 닫기 등 후처리를 담당해요. */
