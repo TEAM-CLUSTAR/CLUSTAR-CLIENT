d7bd3531ab2afce1e5df14caa89edea20d63e8cb
twossu <nadao202@naver.com>
2026-09-21 10:29:53 +0900

    fix: tag-input-field 스크롤바 숨김 스타일 원복

--- apps/client/src/features/tag-popover/tag-input-field/tag-input-field.css.ts
+++ apps/client/src/features/tag-popover/tag-input-field/tag-input-field.css.ts
@@ -30,13 +30,7 @@ export const tagList = style({
   gap: '0.4rem',
   flexWrap: 'nowrap',
   overflowX: 'auto',
-  scrollbarWidth: 'none',
-  msOverflowStyle: 'none',
-  selectors: {
-    '&::-webkit-scrollbar': {
-      display: 'none',
-    },
-  },
+
   flex: 1,
   minWidth: 0,
 });
