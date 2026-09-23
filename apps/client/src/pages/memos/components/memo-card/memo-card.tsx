3f1ac2d7e01606cbc1a24581cf64c0939e3f390f
jogpfls <joerin4177@gmail.com>
2026-09-24 07:29:16 +0900

    fix: 전체 메모에서 updatedAt 기준으로 보여주도록 수정

--- apps/client/src/pages/memos/components/memo-card/memo-card.tsx
+++ apps/client/src/pages/memos/components/memo-card/memo-card.tsx
@@ -12,7 +12,7 @@ export type MemoCardInfoType = {
   memoId: number;
   title: string;
   content: string;
-  createdAt: string;
+  updatedAt: string;
   tagList: components['schemas']['TagResponse'][];
   fileCount: number;
   imageCount: number;
@@ -33,7 +33,7 @@ const MemoCard = ({
   memoId,
   title,
   content,
-  createdAt,
+  updatedAt,
   tagList = [],
   fileCount = 0,
   imageCount = 0,
@@ -93,7 +93,7 @@ const MemoCard = ({
             <span>{imageCount}</span>
           </div>
         </div>
-        <time>{formatDate(createdAt)}</time>
+        <time>{formatDate(updatedAt)}</time>
       </div>
     </article>
   );
